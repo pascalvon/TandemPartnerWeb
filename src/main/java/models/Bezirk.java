23cda9059290adefdc897d66f36b6025a4accd99
Pascal Vonholdt <pascal@example.com>
2018-04-27 13:49:48 +0200

    index.html funktioniert

---
+++ src/main/java/models/Bezirk.java
@@ -0,0 +1,41 @@
+package models;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class Bezirk {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+
+    @OneToMany(mappedBy = "bezirk", cascade = CascadeType.ALL)
+    private Set<Nutzer> nutzerSet;
+
+    @Column(nullable = false)
+    private String bezirkName;
+
+    public Bezirk(){
+        super();
+        nutzerSet = new HashSet<>();
+    }
+
+    public String getBezirkName() {
+        return bezirkName;
+    }
+
+    public void setBezirkName(String bezirkName) {
+        this.bezirkName = bezirkName;
+    }
+
+    @Override
+    public String toString() {
+        return bezirkName;
+    }
+}
