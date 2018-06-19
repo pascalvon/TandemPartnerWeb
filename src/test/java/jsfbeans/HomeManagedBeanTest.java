a9ee5c441fc21aa5064e6b9537c772e8466c97c9
shaker25 <s_schacher@stud.hwr-berlin>
2018-06-19 14:33:20 +0200

    ==> Testklassen für alle Beans erstellt ==> autogenerierte Methoden für Tests erstellen lassen

---
+++ src/test/java/jsfbeans/HomeManagedBeanTest.java
@@ -0,0 +1,38 @@
+package jsfbeans;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HomeManagedBeanTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void showSpracheName() {
+    }
+
+    @Test
+    void acceptMatchanfrage() {
+    }
+
+    @Test
+    void refuseMatchanfrage() {
+    }
+
+    @Test
+    void getNutzer() {
+    }
+
+    @Test
+    void getMatchanfragenModelArrayList() {
+    }
+}
