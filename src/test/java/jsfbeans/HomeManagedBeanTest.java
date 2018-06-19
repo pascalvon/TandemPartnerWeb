8b07ed0b6a3188914b646f848b2b38919fb322e4
JoeBeer <c.cetinkaya@live.de>
2018-06-19 17:43:47 +0200

    - added Mockito to pom - tried first mock (didn't worked)

--- src/test/java/jsfbeans/HomeManagedBeanTest.java
+++ src/test/java/jsfbeans/HomeManagedBeanTest.java
@@ -3,13 +3,20 @@ package jsfbeans;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import sun.rmi.runtime.Log;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class HomeManagedBeanTest {
 
+    // TODO Joe: 19.06.2018 funkt nicht, waren erste Ueberlegungen
+    LoginManagedBean loginManagedBean;
     @BeforeEach
     void setUp() {
+        System.out.println("Test");
+        loginManagedBean = mock(LoginManagedBean.class);
+        loginManagedBean.getNutzer().setMail("test@web.de");
     }
 
     @AfterEach
@@ -18,6 +25,7 @@ class HomeManagedBeanTest {
 
     @Test
     void showSpracheName() {
+        assertEquals("test@web.de", loginManagedBean.getMail());
     }
 
     @Test
