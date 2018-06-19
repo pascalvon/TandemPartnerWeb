0a2037fb180b4114bb2440c667a0597fd79c9b50
shaker25 <s_schacher@stud.hwr-berlin>
2018-06-19 15:10:01 +0200

    ==> Mockito Dependencies hinzugefügt ==> JUnit5 Dependency aktualisiert ==> erstes Mock implementiert (DAO)

--- src/test/java/jsfbeans/HomeManagedBeanTest.java
+++ src/test/java/jsfbeans/HomeManagedBeanTest.java
@@ -1,12 +1,25 @@
 package jsfbeans;
 
+import dao.DAO;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 class HomeManagedBeanTest {
+
+    //19.06.18 Arne: Mock erstellt Objekte, die aus anderen Klassen kommen
+    //19.06.18 Arne: In ihren eigenen Testklassen sollten sie getestet worden sein
+    //19.06.18 Arne: darum ist es zu vernachlässigen, sie hier erneut zu testen.
+    @Mock
+    DAO dao = new DAO();
+
+    @BeforeAll
+    void DbConnection(){
+
+    }
 
     @BeforeEach
     void setUp() {
@@ -17,8 +30,14 @@ class HomeManagedBeanTest {
     }
 
     @Test
-    void showSpracheName() {
+    void showSpracheNameTest() {
     }
+
+    public String showSpracheName(int spracheID) {
+        return dao.findSpracheByID(spracheID).getNameSprache();
+    }
+
+
 
     @Test
     void acceptMatchanfrage() {
