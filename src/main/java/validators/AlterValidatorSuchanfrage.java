674c808d99947b84366ff075dd98579dae60a848
Llois41 <s_ramer@stud.hwr-berlin.de>
2018-06-24 17:01:40 +0200

    JavaDoc AlterValidatorSuchanfrage

--- src/main/java/validators/AlterValidatorSuchanfrage.java
+++ src/main/java/validators/AlterValidatorSuchanfrage.java
@@ -13,6 +13,16 @@ public class AlterValidatorSuchanfrage implements Validator {
     private int alterMin;
     private int alterMax;
 
+    /**
+     * Standardmethode, welche der Validator-Interface zu implementieren aufzwingt
+     * @param facesContext entspricht dem Kontext (Fenster/Seite)
+     * @param uiComponent enspricht der Komponente, an welcher der Validator hängt (Button/InputText...)
+     * @param o beinhaltet die Anwendereingaben der Komponente (Datentyp: Object)
+     * @throws ValidatorException zeigt an, dass die Methode ValidatorExceptions werfen kann
+     *
+     * Die Eingabefelder der Altersparamter der Suchanfrage werden auf entsprechenden Inhalt geprüft.
+     * Beim Anschlagen des Validators wird die entsprechende Fehlermeldung ausgegeben
+     */
     @Override
     public void validate(FacesContext facesContext, UIComponent uiComponent, Object o) throws ValidatorException {
 
